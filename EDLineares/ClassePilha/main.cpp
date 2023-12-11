a9449d1647da17e8f8dea09b12d07aff983007c0
Lev Kamenev <kamenev@pop-os.localdomain>
2023-12-10 21:12:26 -0300

    Torre de Hanoi

--- EDLineares/ClassePilha/main.cpp
+++ EDLineares/ClassePilha/main.cpp
@@ -1,4 +1,16 @@
 #include "stack.h"
 #include <iostream>
 
+int main(void){
+  constexpr int numDisco = 3;
+  
+  Pilha<int, numDisco> inicial;
+  for (int i = numDisco; i >= 1; i--){
+    inicial.empilha(i);
+  }
+
+
+
+  return 0;
+}
 

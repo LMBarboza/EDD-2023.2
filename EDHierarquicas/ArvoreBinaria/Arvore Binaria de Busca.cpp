c4493df1854a1dc94705ed86dab0ffa2fd7bb0c8
a.n.d.r.e.l.u.i.s <89691263+ndrlsae@users.noreply.github.com>
2023-12-11 22:24:57 -0300

    Add files via upload

---
+++ EDHierarquicas/ArvoreBinaria/Arvore Binaria de Busca.cpp
@@ -0,0 +1,79 @@
+// Arvore Binaria de Busca.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+#include <stack> // tirar dps
+
+template <typename TAB>
+class NoArv {
+public:
+    NoArv(TAB i = NULL) { TAB valor = i; }
+    NoArv* right;
+    NoArv* left;
+};
+
+
+template <typename TABa>
+class ArvBB {
+public:
+    ArvBB() {NoArv<TABa> rad;}
+    ArvBB(TABa no1) { NoArv<TABa> rad(no1); }
+
+    void add(TABa obj) {
+        No_Arv<TABa> new_no(obj);
+        No_Arv<TABa> Itr = rad;
+        while (true) {
+            cout << "loop infinito" << endl;
+            if (new_no < Itr) {
+                cout << "new_no é menor que Itr=" << Itr.valor << endl;
+                if (Itr->left != NULL) { Itr = Itr->left; };
+
+                else { Itr->left = &new_no; break; };
+            };
+            else if (new_no > Itr) {
+                cout << "new_no é maior que Itr=" << Itr.valor << endl;
+                if (Itr->right != NULL) { Itr = Itr->right; };
+                else { Itr->right = &new_no; break; };
+            }
+        }
+
+    }
+
+
+};
+
+//kk ta
+
+/*
+template <typename TABp>
+void printArv(ArvBB<TABp> arv) {
+    NoArv<TABp> Itr = arv.rad;
+    stack<NoArv> PArv;
+    PArv.push(Itr);
+    /*
+    while (true) {
+        cout << "looping infinito" << edl;
+        if (Itr->left != NULL) {
+            cout << "lp no if" << endl;
+            Itr = Itr->left;
+        }
+        else (Itr->right != NULL) {
+            cout << Itr.valor << endl;
+            cout << "lp no else" << endl;
+            Itr 
+        }
+    }
+    while ((Itr->left != NULL) || (Itr->right != NULL)) {
+        
+        if (Itr->left != NULL) { Itr = Itr->left; PArv.push(Itr); break; }
+        else { cout << (PArv.pop()).valor << endl; Itr  = PArv.top() }
+
+        if(Itr->right != NULL) {Itr = Itr->}        
+    }
+}*/
+
+
+

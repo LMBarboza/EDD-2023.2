a592e69053612535a74f76e28612e337c92e28d4
a.n.d.r.e.l.u.i.s <89691263+ndrlsae@users.noreply.github.com>
2023-12-22 09:35:07 -0300

    Add files via upload

--- EDHierarquicas/ArvoreBinaria/ArvoreBinariadeBusca.cpp
+++ EDHierarquicas/ArvoreBinaria/ArvoreBinariadeBusca.cpp
@@ -1,13 +1,16 @@
-/*
 #include <iostream>
 #include<stdio.h>
 #include<stdlib.h>
 
+#include "stack.h"
+
+using namespace std;
 
 template <typename TAB>
 class NoArv {
 public:
-    NoArv(TAB i = NULL) { TAB valor = i; }
+    TAB valor;
+    NoArv(TAB i = NULL) { valor = i; }
     NoArv* right;
     NoArv* left;
 };
@@ -16,61 +19,179 @@ public:
 template <typename TABa>
 class ArvBB {
 public:
-    ArvBB() {NoArv<TABa> rad;}
-    ArvBB(TABa no1) { NoArv<TABa> rad(no1); }
+    NoArv<TABa> rad;
+    ArvBB() { }
+    ArvBB(TABa no1) { rad = NoArv<TABa>(no1); rad.left = nullptr; rad.right = nullptr; }
+
+    NoArv<TABa>* search(TABa obj) {
+        NoArv<TABa>* Itr = &rad;
+
+        while (Itr->valor != obj) {
+            if (obj < Itr->valor && Itr->left != NULL) {
+
+                Itr = Itr->left;
+            }
+            else if (obj > Itr->valor && Itr->right != NULL) {
+                Itr = Itr->right;
+            }
+            else {
+                cout << "Endereço nao encontrado" << endl;
+                break;
+            }
+        }
+        return Itr;
+
+    }
 
     void add(TABa obj) {
-        No_Arv<TABa> new_no(obj);
-        No_Arv<TABa> Itr = rad;
-        while (true) {
-            cout << "loop infinito" << endl;
-            if (new_no < Itr) {
-                cout << "new_no é menor que Itr=" << Itr.valor << endl;
-                if (Itr->left != NULL) { Itr = Itr->left; }
+        if (rad.valor == NULL) {
+            cout << "adicionou " << obj << endl;
+            rad.valor = obj;
+            return;
+        }
 
-                else { Itr->left = &new_no; break; };
+        NoArv<TABa>* new_no;
+        new_no = new NoArv<TABa>(obj);
+        new_no->left = nullptr;
+        new_no->right = nullptr;
+
+
+        NoArv<TABa>* Itr = &rad;
+
+        while (Itr->left != nullptr || Itr->right != nullptr) {
+
+            if (new_no->valor < Itr->valor) {
+                if (Itr->left != nullptr) {
+                    Itr = Itr->left;
+                }
+
+                else {
+
+                    Itr->left = &(*new_no);
+                    return;
+                };
             }
-            else if (new_no > Itr) {
-                cout << "new_no é maior que Itr=" << Itr.valor << endl;
-                if (Itr->right != NULL) { Itr = Itr->right; }
-                else { Itr->right = &new_no; break; };
+            else if (new_no->valor > Itr->valor) {
+
+
+                if (Itr->right != nullptr) { Itr = Itr->right; }
+
+                else {
+                    Itr->right = &(*new_no);
+                    return;
+                };
             }
+        }
+
+        if (obj < Itr->valor) {
+
+            Itr->left = &(*new_no);
+            return;
+        }
+        else {
+
+            Itr->right = &(*new_no);
+            return;
         }
 
     }
 
+    void remove(TABa obj) {
+        if (search(obj) == NULL) { return; }
+
+        NoArv<TABa>* Itr = &rad;
+        NoArv<TABa>* Itr2 = &rad;
+        bool noLeft = true;
+
+        while (Itr->valor != obj) {
+            Itr2 = Itr;
+            if (obj < Itr->valor) {
+                Itr = Itr->left;
+                noLeft = true;
+            }
+            else { Itr = Itr->right; noLeft = false; }
+        }
+
+        if (Itr->left == NULL && Itr->right == NULL) {
+            if (Itr == &rad) { rad = NULL; }
+            else if (noLeft) { Itr2->left = NULL; }
+            else { Itr2->right = NULL; }
+        }
+
+        else if (Itr->right == NULL) {
+            if (Itr == &rad) { rad = *Itr->left; }
+            else if (noLeft) { Itr2->left = Itr->left; }
+            else { Itr2->right = Itr->left; }
+
+        }
+
+        else if (Itr->left == NULL) {
+            if (Itr == &rad) { rad = *Itr->right; }
+            else if (noLeft) { Itr2->left = Itr->right; }
+            else { Itr2->right = Itr->left; }
+        }
+
+        else {
+            NoArv<TABa>* prox = prox_no(Itr);
+
+            if (Itr == &rad) { rad = *prox; }
+            else if (noLeft) { Itr2->left = prox; }
+            else { Itr2->right = prox; }
+
+            prox->left = Itr->left;
+        }
+        return;
+    }
+
+    void printPreord() { printPreord2(&rad); }
+
+    void printOrd() { printOrd2(&rad); }
+
+    void printPosOrd() { printPosord2(&rad); }
+
+private:
+    NoArv<TABa>* prox_no(NoArv<TABa>* obj) {
+        NoArv<TABa>* Itr = obj;
+        NoArv<TABa>* Itr2 = obj;
+        NoArv<TABa>* Itr3 = obj->right;
+
+        while (Itr3 != NULL) {
+            Itr2 = Itr;
+            Itr = Itr3;
+            Itr3 = Itr3->right;
+        }
+
+        if (Itr != obj->right) {
+            Itr2->left = Itr->right;
+
+            Itr->right = obj->right;
+        }
+        return Itr;
+    }
+
+    void printPreord2(NoArv<TABa>* Itr) {
+        if (Itr != NULL) {
+            cout << Itr->valor << endl;
+            printPreord2(Itr->left);
+            printPreord2(Itr->right);
+        }
+    }
+
+    void printPosord2(NoArv<TABa>* Itr) {
+        if (Itr != NULL) {
+            printPosord2(Itr->left);
+            printPosord2(Itr->right);
+            cout << Itr->valor << endl;
+        }
+    }
+
+    void printOrd2(NoArv<TABa>* Itr) {
+        if (Itr != NULL) {
+            printOrd2(Itr->left);
+            cout << Itr->valor << endl;
+            printOrd2(Itr->right);
+        }
+
+    }
 
 };
-
-//kk ta
-
-/*
-template <typename TABp>
-void printArv(ArvBB<TABp> arv) {
-    NoArv<TABp> Itr = arv.rad;
-    stack<NoArv> PArv;
-    PArv.push(Itr);
-    /*
-    while (true) {
-        cout << "looping infinito" << edl;
-        if (Itr->left != NULL) {
-            cout << "lp no if" << endl;
-            Itr = Itr->left;
-        }
-        else (Itr->right != NULL) {
-            cout << Itr.valor << endl;
-            cout << "lp no else" << endl;
-            Itr 
-        }
-    }
-    while ((Itr->left != NULL) || (Itr->right != NULL)) {
-        
-        if (Itr->left != NULL) { Itr = Itr->left; PArv.push(Itr); break; }
-        else { cout << (PArv.pop()).valor << endl; Itr  = PArv.top() }
-
-        if(Itr->right != NULL) {Itr = Itr->}        
-    }
-}*/
-
-
-
--- EDHierarquicas/ArvoreBinaria/ArvoreBinariadeBusca.h
+++ EDHierarquicas/ArvoreBinaria/ArvoreBinariadeBusca.h
@@ -2,6 +2,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#include "stack.h"
+
 using namespace std;
 
 template <typename TAB>
@@ -18,8 +20,28 @@ template <typename TABa>
 class ArvBB {
 public:
     NoArv<TABa> rad;
-    ArvBB() { rad = NoArv<TABa>; }
-    ArvBB(TABa no1) { rad = NoArv<TABa>(no1); }
+    ArvBB() { }
+    ArvBB(TABa no1) { rad = NoArv<TABa>(no1); rad.left = nullptr; rad.right = nullptr; }
+
+    NoArv<TABa>* search(TABa obj) {
+        NoArv<TABa>* Itr = &rad;
+
+        while (Itr->valor != obj) {
+            if (obj < Itr->valor && Itr->left != NULL){
+
+                Itr = Itr->left;
+            }
+            else if (obj > Itr->valor && Itr->right != NULL) {
+                Itr = Itr->right;
+            }
+            else {
+                cout << "Endere�o nao encontrado" << endl;
+                break;
+            }
+        }
+        return Itr;
+
+    }
 
     void add(TABa obj) {
         if (rad.valor == NULL) {
@@ -28,40 +50,147 @@ public:
             return;
         }
 
-        NoArv<TABa> new_no(obj);
+        NoArv<TABa>* new_no;
+        new_no = new NoArv<TABa>(obj);
+        new_no->left = nullptr;
+        new_no->right = nullptr;
 
 
         NoArv<TABa>* Itr = &rad;
-        while (Itr->left != NULL && Itr->right != NULL) {
-            cout << "loop infinito" << endl;
-            if (new_no.valor < Itr->valor) {
-                cout << "new_no " << obj << "é menor que Itr=" << Itr->valor << endl;
-                if (Itr->left != NULL) { Itr = Itr->left; cout << "entrei aqui" << endl; }
+        
+        while (Itr->left != nullptr || Itr->right != nullptr) {
 
-                else { Itr->left = &new_no; cout << new_no.left->valor << " left valor and no valor " << new_no.valor << endl; };
+            if (new_no->valor < Itr->valor) {
+                    if (Itr->left != nullptr) {
+                    Itr = Itr->left;                 }
+
+                else {
+                   
+                    Itr->left = &(*new_no);  
+                    return;
+                };
             }
-            else if (new_no.valor > Itr->valor) {
-                cout << "new_no " << obj << "é maior que Itr=" << Itr->valor << endl;
+            else if (new_no->valor > Itr->valor) {
+                
 
-                if (Itr->right != NULL) { Itr = Itr->right; }
+                if (Itr->right != nullptr) {Itr = Itr->right; }
 
-                else { Itr->right = &new_no; };
+                else {
+                    Itr->right = &(*new_no); 
+                    return;
+                };
             }
-            //cout << "radL " << rad.left->valor << endl;
-            //cout << "radR" << rad->right->valor << endl;
         }
 
-        cout << "alo" << endl;
         if (obj < Itr->valor) {
-            Itr->left = &new_no;
+
+            Itr->left = &(*new_no);
+            return;
         }
         else {
-            Itr->right = &new_no;
+            
+            Itr->right = &(*new_no);
+            return;
         }
+
     }
 
+    void remove(TABa obj) {
+        if (search(obj) == NULL) { return; }
+
+        NoArv<TABa>* Itr = &rad;
+        NoArv<TABa>* Itr2 = &rad;
+        bool noLeft = true;
+
+        while (Itr->valor != obj) {
+            Itr2 = Itr;
+            if (obj < Itr->valor) {
+                Itr = Itr->left;
+                noLeft = true;
+            }
+            else { Itr = Itr->right; noLeft = false; }
+        }
+
+        if (Itr->left == NULL && Itr->right == NULL) {
+            if (Itr == &rad) { rad = NULL; }
+            else if (noLeft) { Itr2->left = NULL; }
+            else { Itr2->right = NULL; }
+        }
+
+        else if (Itr->right == NULL) {
+            if (Itr == &rad) { rad = *Itr->left; }
+            else if (noLeft) { Itr2->left = Itr->left; }
+            else { Itr2->right = Itr->left; }
+
+        }
+
+        else if (Itr->left == NULL) {
+            if (Itr == &rad) { rad = *Itr->right; }
+            else if (noLeft) { Itr2->left = Itr->right; }
+            else { Itr2->right = Itr->left; }
+        }
+
+        else {
+            NoArv<TABa>* prox = prox_no(Itr);
+            
+            if (Itr == &rad) { rad = *prox; }
+            else if (noLeft) { Itr2->left = prox; }
+            else { Itr2->right = prox; }
+
+            prox->left = Itr->left;
+        }
+        return;
+        }
+
+    void printPreord() { printPreord2(&rad); } 
+
+    void printOrd() { printOrd2(&rad); }
+    
+    void printPosOrd() { printPosord2(&rad); }
+
+ private: 
+     NoArv<TABa>* prox_no(NoArv<TABa>* obj) {
+     NoArv<TABa>* Itr = obj;
+     NoArv<TABa>* Itr2 = obj;
+     NoArv<TABa>* Itr3 = obj->right;
+
+     while (Itr3 != NULL) {
+         Itr2 = Itr;
+         Itr = Itr3;
+         Itr3 = Itr3->right;
+     }
+
+     if (Itr != obj->right) {
+         Itr2->left = Itr->right;
+
+         Itr->right = obj->right;
+     }
+     return Itr;
+ }
+
+     void printPreord2(NoArv<TABa>* Itr) {
+         if (Itr != NULL) {
+             cout << Itr->valor << endl;
+             printPreord2(Itr->left);
+             printPreord2(Itr->right);
+         }
+     }
+
+     void printPosord2(NoArv<TABa>* Itr) {
+         if (Itr != NULL) {
+             printPosord2(Itr->left);
+             printPosord2(Itr->right);
+             cout << Itr->valor << endl;
+         }
+     }
+
+     void printOrd2(NoArv<TABa>* Itr) {
+         if (Itr != NULL) {
+             printOrd2(Itr->left);
+             cout << Itr->valor << endl;
+             printOrd2(Itr->right);
+         }
+
+     }
+
 };
-
-
-
-
